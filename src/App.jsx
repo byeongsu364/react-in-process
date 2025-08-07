07cb5146ef0a16993b4a7be6069fc72406085674
byeongsu364 <kbsyjysbs@gmail.com>
2025-08-07 14:12:32 +0900

    1

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,15 @@
 import './App.css'
-import Button from './components/Button'
-import { Input } from './components/Input'
+import FocusInput from './components/FocusInput'
+import Counter from './components/Counter'
+import inputTracker from './components/inputTracker'
 
 function App() {
 
   return (
     <div>
-      <h1>hello React!</h1>
-      <Button text = {'메일'} color = {'red'}/>
-      <Button text = {'카페'} color = {'blue'}/>
-      <Button text = {'블로그'} color = {'orange'}/>
-      <Button text = {'기본값 설정'}/>
-      <Button text = {'자식요소 설정'} color = {'pink'}>
-      <strong>중요</strong>
-      </Button>
-
-      <Input
-        inputValue={'hello react'}
-        title={'input title'}
-        placeholder={'입력하세요'} />
+      <FocusInput/>
+      <Counter/>
+      <inputTracker/>
     </div>
   )
 }
