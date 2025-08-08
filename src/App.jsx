8501ba2d72ad9c7c38ec02def10e0e926099c973
byeongsu364 <kbsyjysbs@gmail.com>
2025-08-08 10:19:54 +0900

    h

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
-import './App.css'
-import LifeCycle from './components/LifeCycle'
+import Controller from'./components/Controller'
+import Viewer from'./components/Viewer'
+import { useState } from 'react'
+import './App.scss'
 
 function App() {
 
+  const [count, setCount] = useState(0)
+
+  const onClickButton =(value)=> {
+    setCount(count + value)
+  }
+
   return (
-    <div>
-      <LifeCycle />
+    <div className='App'>
+      <h1>Simple Counter</h1>
+      {count}
+      <section>
+        <Viewer count={count} />
+      </section>
+      <section>
+        <Controller onClickButton={onClickButton}/>
+      </section>
     </div>
   )
 }
