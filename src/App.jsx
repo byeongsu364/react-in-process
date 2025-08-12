5ccf6f24d8047116421d9669c34bfb1d17d8feb5
byeongsu364 <kbsyjysbs@gmail.com>
2025-08-12 15:11:45 +0900

    dd

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Counter from './components/Counter'
+import TodoList from './components/TodoList'
 import './App.css'
 
 function App() {
@@ -6,7 +6,7 @@ function App() {
 
   return (
     <div>
-      <Counter/>
+      <TodoList /> 
     </div>
   )
 }
